b76e315a8c71771730924a307634fe4ce71b976d
redf0x7c5 <redf0x7c5@gmail.com>
2014-04-12 02:56:06 +0100

    OGAD 01: inicio; criação da classe stick

---
+++ games/ogad_01_stick_n_ball/ogad_01_stick_n_ball/app.js
@@ -0,0 +1,36 @@
+﻿var Stick = (function () {
+    function Stick(element) {
+        this.element = element;
+
+        this.max_angle = 15;
+        this.prev_x_pos = 0;
+    }
+    Stick.prototype.onMouseMove = function (event) {
+        var x = event.pageX;
+
+        this.speed = this.prev_x_pos - x;
+
+        this.angle = Math.max(-this.max_angle, Math.min(this.max_angle, this.speed));
+
+        this.element.style.left = x.toString() + "px";
+
+        //this.element.setAttribute("style", "-moz-transform: rotate(" + this.angle.toString() + "deg)" );
+        this.element.style.MozTransform = "rotate(" + this.angle.toString() + "deg)";
+
+        this.prev_x_pos = x;
+    };
+    return Stick;
+})();
+
+var Ball = (function () {
+    function Ball() {
+    }
+    return Ball;
+})();
+
+var stick;
+
+window.onload = function () {
+    stick = new Stick(document.getElementById("stick"));
+};
+//# sourceMappingURL=app.js.map
---
+++ games/ogad_01_stick_n_ball/ogad_01_stick_n_ball/app.ts
@@ -0,0 +1,41 @@
+﻿
+class Stick {
+    element: HTMLElement;
+
+    angle: number;
+    speed: number;
+
+    max_angle: number;
+    prev_x_pos: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+
+        this.max_angle = 15;
+        this.prev_x_pos = 0;
+    }
+
+    onMouseMove(event: MouseEvent) {
+        var x = event.pageX;
+
+        this.speed = this.prev_x_pos - x;
+
+        this.angle = Math.max(-this.max_angle, Math.min(this.max_angle, this.speed));
+
+        this.element.style.left = x.toString() + "px";
+        //this.element.setAttribute("style", "-moz-transform: rotate(" + this.angle.toString() + "deg)" );
+        this.element.style.MozTransform = "rotate(" + this.angle.toString() + "deg)";
+
+        this.prev_x_pos = x;
+    }
+}
+
+class Ball {
+
+}
+
+var stick: Stick;
+
+window.onload = () => {
+    stick = new Stick(document.getElementById("stick"));
+};
