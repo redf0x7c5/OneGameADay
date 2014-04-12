89146fdc97cfebcb86e6a1a1453dcfc5c1c44556
redf0x7c5 <redf0x7c5@gmail.com>
2014-04-12 15:25:51 +0100

    mudança para sistema com Canvas

--- games/ogad_01_stick_n_ball/ogad_01_stick_n_ball/app.js
+++ games/ogad_01_stick_n_ball/ogad_01_stick_n_ball/app.js
@@ -1,21 +1,71 @@
-﻿var Stick = (function () {
-    function Stick(element) {
-        this.element = element;
+﻿var GameCanvas = (function () {
+    function GameCanvas() {
+        var _this = this;
+        this.entities = new Array();
+        this.time = new GameTime();
 
+        var stick = new Stick();
+        this.entities.push(stick);
+
+        this.canvasElement = document.getElementById("game-canvas");
+        this.canvasElement.onmousemove = function (e) {
+            return stick.onMouseMove(e);
+        };
+
+        this.canvas = this.canvasElement.getContext('2d');
+        window.requestAnimationFrame(function (dt) {
+            return _this.update(dt);
+        });
+    }
+    GameCanvas.prototype.update = function (dt) {
+        var _this = this;
+        this.time.deltaTime = dt;
+        this.time.totalTime = this.time.totalTime + dt;
+
+        this.canvas.clearRect(0, 0, 600, 400);
+        this.entities.forEach(function (ge) {
+            ge.update(_this.time);
+            ge.draw(_this.canvas);
+        });
+
+        window.requestAnimationFrame(function (dt) {
+            return _this.update(dt);
+        });
+    };
+    return GameCanvas;
+})();
+
+var GameTime = (function () {
+    function GameTime() {
+        this.totalTime = 0;
+        this.deltaTime = 0;
+    }
+    return GameTime;
+})();
+
+var Stick = (function () {
+    function Stick() {
         this.max_angle = 15;
         this.prev_x_pos = 0;
+
+        this.x = 0;
+        this.y = 300;
     }
+    Stick.prototype.update = function (dt) {
+    };
+
+    Stick.prototype.draw = function (gc) {
+        gc.fillStyle = "rgb(200,0,0)";
+        gc.fillRect(this.x, this.y, 100, 20);
+    };
+
     Stick.prototype.onMouseMove = function (event) {
         var x = event.pageX;
+        this.x = x;
 
         this.speed = this.prev_x_pos - x;
 
         this.angle = Math.max(-this.max_angle, Math.min(this.max_angle, this.speed));
-
-        this.element.style.left = x.toString() + "px";
-
-        //this.element.setAttribute("style", "-moz-transform: rotate(" + this.angle.toString() + "deg)" );
-        this.element.style.MozTransform = "rotate(" + this.angle.toString() + "deg)";
 
         this.prev_x_pos = x;
     };
@@ -28,9 +78,9 @@ var Ball = (function () {
     return Ball;
 })();
 
-var stick;
+var gameCanvas;
 
 window.onload = function () {
-    stick = new Stick(document.getElementById("stick"));
+    gameCanvas = new GameCanvas();
 };
 //# sourceMappingURL=app.js.map
--- games/ogad_01_stick_n_ball/ogad_01_stick_n_ball/app.ts
+++ games/ogad_01_stick_n_ball/ogad_01_stick_n_ball/app.ts
@@ -1,6 +1,65 @@
 ﻿
-class Stick {
-    element: HTMLElement;
+class GameCanvas {
+    entities: GameEntity[];
+    time: GameTime;
+    canvas: CanvasRenderingContext2D;
+    canvasElement: HTMLCanvasElement;
+
+    constructor() {
+        this.entities = new Array<GameEntity>();
+        this.time = new GameTime();
+
+        var stick: Stick = new Stick();
+        this.entities.push(stick);
+
+        this.canvasElement = <HTMLCanvasElement> document.getElementById("game-canvas")
+        this.canvasElement.onmousemove = (e) => stick.onMouseMove(e);
+
+        this.canvas = this.canvasElement.getContext('2d');
+        window.requestAnimationFrame((dt) => this.update(dt));
+    }
+
+    update(dt: number) {
+        this.time.deltaTime = dt;
+        this.time.totalTime = this.time.totalTime + dt;
+
+        this.canvas.clearRect(0, 0, 600, 400);
+        this.entities.forEach((ge: GameEntity) => { ge.update(this.time); ge.draw(this.canvas); } );
+
+        window.requestAnimationFrame((dt) => this.update(dt));
+    }
+}
+
+class GameTime {
+    totalTime: number;
+    deltaTime: number;
+
+    constructor() {
+        this.totalTime = 0;
+        this.deltaTime = 0;
+    }
+}
+
+interface GameEntity {
+    x: number;
+    y: number;
+
+    sx: number;
+    sy: number
+
+    angle: number;
+    speed: number;
+
+    update(dt: GameTime);
+    draw(gc: CanvasRenderingContext2D);
+}
+
+class Stick implements GameEntity {
+    x: number;
+    y: number;
+
+    sx: number;
+    sy: number;
 
     angle: number;
     speed: number;
@@ -8,23 +67,30 @@ class Stick {
     max_angle: number;
     prev_x_pos: number;
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-
+    constructor() {
         this.max_angle = 15;
         this.prev_x_pos = 0;
+
+        this.x = 0;
+        this.y = 300;
+    }
+
+    update(dt: GameTime) {
+        
+    }
+
+    draw(gc: CanvasRenderingContext2D) {
+        gc.fillStyle = "rgb(200,0,0)";
+        gc.fillRect(this.x, this.y, 100, 20);
     }
 
     onMouseMove(event: MouseEvent) {
         var x = event.pageX;
+        this.x = x;
 
         this.speed = this.prev_x_pos - x;
 
         this.angle = Math.max(-this.max_angle, Math.min(this.max_angle, this.speed));
-
-        this.element.style.left = x.toString() + "px";
-        //this.element.setAttribute("style", "-moz-transform: rotate(" + this.angle.toString() + "deg)" );
-        this.element.style.MozTransform = "rotate(" + this.angle.toString() + "deg)";
 
         this.prev_x_pos = x;
     }
@@ -34,8 +100,8 @@ class Ball {
 
 }
 
-var stick: Stick;
+var gameCanvas: GameCanvas;
 
 window.onload = () => {
-    stick = new Stick(document.getElementById("stick"));
+    gameCanvas = new GameCanvas();
 };
